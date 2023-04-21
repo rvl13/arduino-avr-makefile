2ee29428d9a2f86e191a7b4e681dc47296a30b3c
rvl13 <111491316+rvl13@users.noreply.github.com>
2023-04-21 23:46:30 +0530

    Comment correction

--- Arduino_makefile/Mega2560/blink_plain_C/src/blink_led.c
+++ Arduino_makefile/Mega2560/blink_plain_C/src/blink_led.c
@@ -4,19 +4,19 @@
 #define MS_DELAY 500
 
 int main (void) {
-    /*Set to one the fifth bit of DDRB to one
+    /*Set to one the seventh bit of DDRB to one
     **Set digital pin 13 to output mode */
     DDRB |= _BV(DDB7);
 
     while(1) {
-        /*Set to one the fifth bit of PORTB to one
+        /*Set to one the seventh bit of PORTB to one
         **Set to HIGH the pin 13 */
         PORTB |= _BV(PORTB7);
 
         /*Wait 3000 ms */
         _delay_ms(MS_DELAY);
 
-        /*Set to zero the fifth bit of PORTB
+        /*Set to zero the seventh bit of PORTB
         **Set to LOW the pin 13 */
         PORTB &= ~_BV(PORTB7);
 
